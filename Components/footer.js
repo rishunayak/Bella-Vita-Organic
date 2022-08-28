05e493c24e76f861b82c9fb959f1c15d9b5cac42
Bishwajit0759 <bishwajitparida530@hotmail.com>
2022-08-28 09:01:19 +0530

    hair care added

---
+++ Components/footer.js
@@ -0,0 +1,60 @@
+function footer(){
+    return `<div id="fotbox">
+    
+    <div>
+        <h5>INFORMATION</h5>
+          <p>Blogs</p>
+          <p>Newsroom</p>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Refund and Return</p>
+          <p>Shipping Policy</p>
+    </div>
+
+    <div>
+        <h5>SUPPORT</h5>
+        <p>About Us</p>
+        <p>Contact Us</p>
+        <p>Order Tracking</p>
+        <p>All Products</p>
+        <p>FAQ</p>
+    </div>
+
+    <div>
+        <h5>CONTACT US</h5>
+        <p>Office Location: Plot no. 417, Udyog Vihar Phase III, Gurgaon,Haryana ,India</p>
+        <p>shop@bellavitaorganic.com</p>
+        <p>+91 9311732440</p>
+        <p>Timing:9:00 AM To 9:00 PM , Monday To Sunday</p>
+
+    </div>
+
+    <div>
+        <h5>OUR NEWSLETTER</h5>
+        <p>Sign up here to get the latest news, updates and special offers delivered to your inbox.</p>
+        <p>Plus, you'll be the first to know about our discounts!</p>
+        <p><input type="text" name="" id="" placeholder=" Enter Your Email"  style="width: 200px" ></p>
+        <p><Button>SUBMIT</Button></p>
+    </div>
+   
+   </div>
+   <div id="copyright">
+     <div>
+         <div>
+           <img src="https://cdn.shopify.com/s/files/1/0054/6665/2718/files/payments-stripe-footer_256x256_e_400x.png?v=1650369513" alt="">
+         </div>
+        <div id="footericon">
+          <i class="fab fa-instagram"></i>
+          <i class="fab fa-facebook"></i>
+          <i class="fab fa-youtube"></i>
+          <i class="fab fa-twitter"></i>
+          <i class="fab fa-pinterest"></i>
+        </div>
+        <div>
+           © 2022, Bella Vita Organic
+        </div>
+    
+    </div>`
+}
+
+export  {footer};
